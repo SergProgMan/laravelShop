c90bdf338b34e453195b8e45234d68c4f46efb40
SergProgMan <semibratyev.com>
2018-12-13 15:20:12 +0200

    BackOffice and isAdmin

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware(['auth','isAdmin'])->namespace('BackOffice')->prefix('control-panel')->group(function(){
+    Route::resource('products','ProductsController');
+});
